f862a28a990439834eaf37726cb78e67c6226ba9
Sylvain Hamann <sylvain.hamann@ubisoft.com>
2018-12-20 13:32:28 -0500

    fix css flickering

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
+import Layout from './src/components/Layout';
 import theme from './theme';
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={theme}>{element}</ThemeProvider>
+);
+
+export const wrapPageElement = ({ element, props }) => (
+  <Layout {...props}>{element}</Layout>
 );
