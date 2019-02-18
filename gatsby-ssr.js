fa3cc5520b07d02581dd6300918dc0c4065facd8
Sylvain Hamann <sylvain.hamann@ubisoft.com>
2019-02-18 09:53:52 -0500

    Fix build issue

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,2 +1,5 @@
-export { wrapRootElement } from './src/components/wrapRootElement';
+import { wrapRootElement as exportMeAsWrapRootElement } from './src/components/wrapRootElement';
 export { wrapPageElement } from './src/components/wrapPageElement';
+
+// The build is failing if we directly export like we do with wrapPageElement
+export const wrapRootElement = exportMeAsWrapRootElement;
