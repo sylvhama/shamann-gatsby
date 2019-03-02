284004b09e01092bc7ff940c22cc24cc2e285a7d
Sylvain Hamann <sylvhama@gmail.com>
2019-03-02 18:51:44 -0500

    Remove /

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ module.exports = {
     author: '@sylvhama',
     navLinks: [
       { name: 'About Me', path: '/' },
-      { name: 'Relevant Experience', path: '/relevant-experience/' },
-      { name: 'Showcase', path: '/showcase/' },
-      { name: 'Talks & OSS', path: '/talks-open-source/' },
+      { name: 'Relevant Experience', path: '/relevant-experience' },
+      { name: 'Showcase', path: '/showcase' },
+      { name: 'Talks & OSS', path: '/talks-open-source' },
     ],
   },
   plugins: [
