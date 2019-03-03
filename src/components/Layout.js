caf5f279c7476cd78f4e84d6a81231989f2bbf1d
Sylvain Hamann <sylvhama@gmail.com>
2019-03-03 15:15:59 -0500

    Hide nav

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -38,7 +38,7 @@ const HR = styled.hr`
   opacity: 0.5;
 `;
 
-const Layout = ({ children }) => (
+const Layout = ({ children, ...rest }) => (
   <StaticQuery
     query={graphql`
       query SiteTitleQuery {
@@ -61,7 +61,9 @@ const Layout = ({ children }) => (
     }) => (
       <>
         <Header title={title} description={description} />
-        <Nav links={navLinks} />
+        {rest['*'] !== 'offline-plugin-app-shell-fallback' && (
+          <Nav links={navLinks} />
+        )}
         <Wrapper>
           {children}
           <HR />
