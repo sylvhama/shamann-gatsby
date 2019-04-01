7d62f5e25f465d6cadd92797f819784b65cb04e1
Sylvain Hamann <sylvhama@gmail.com>
2019-04-01 16:48:10 -0400

    Update relevant-experience.js

--- src/pages/relevant-experience.js
+++ src/pages/relevant-experience.js
@@ -10,12 +10,12 @@ export default () => (
       I graduated from <em>Lumière University Lyon 2</em> in 2013. I have a
       Bachelor’s degree <small>(in France we call that a Licence)</small> in
       statistics and computer science and a <strong>Master’s degree</strong> in
-      multimedia.
+      multimedia. During my studies, I have interned at four different companies in France,
+      Luxembourg and South Korea. I am really used to work in a multicultural
+      environment.
     </Paragraph>
     <Paragraph>
-      During my studies, I have interned at four different companies in France,
-      Luxembourg and South Korea. I am really used to work in a multicultural
-      environment. After I graduated, I have worked as a web developer for two
+      After I graduated, I have worked as a web developer for two
       years at Asiance, a digital agency located in <strong>Seoul</strong>. I
       also have <strong>worked in Tokyo</strong> as a web developer for
       two digital agencies. I initially joined Netwise for eight months and then
@@ -25,13 +25,12 @@ export default () => (
       Lausanne University Hospital's interactive map.
     </Paragraph>
     <Paragraph>
-      Nowadays I work at <strong>Ubisoft Montreal</strong> in Canada as a web
-      developer. My main activity is to maintain and develop features for the
-      Ubisoft Club applications on PlayStation 4 and Xbox One. I also have
-      created a platform to easily develop internal tools that now have two
-      developers working full time on it. Plus I have worked on web views that
-      summarize your Club progression visible from the main menu of Assassin's
-      Creed Origins and Odyssey.
+      I aslo have worked at <strong>Ubisoft Montreal</strong> in Canada as a web
+      developer. My main activity was to maintain and develop features for the
+      Ubisoft Club single-page applications on PlayStation 4 and Xbox One. I also have
+      created a Progressive Web App used to easily develop internal tools. Plus I have 
+      worked on web views tha summarize your Club progression visible from the main 
+      menu of Assassin's Creed Origins and Odyssey.
     </Paragraph>
   </>
 );
