578e7516e2e500c14730dfa0044df01f14ddd411
Sylvain Hamann <sylvhama@gmail.com>
2019-09-28 11:36:39 -0400

    Update talks-open-source.js

--- src/pages/talks-open-source.js
+++ src/pages/talks-open-source.js
@@ -29,12 +29,8 @@ export default () => (
       (Seoul, Tokyo and Montreal) or taking care of developers debuting in web
       development. E.g. I've made a list of{' '}
       <Anchor href="https://github.com/sylvhama/modern-js">free content</Anchor>{' '}
-      for people debuting with ES6/2015 and React. All interns I've worked with
-      have said I was a good mentor{' '}
-      <span role="img" aria-label="flushed face">
-        😳
-      </span>
-      . I also enjoy doing internal presentations or sharing articles to
+      for people debuting with ES6/2015 and React.
+      I also enjoy doing internal presentations or sharing articles to
       introduce new concepts or libraries to my teammates. I am also used to do
       pair programming to solve problems in a more efficient way.
     </Paragraph>
