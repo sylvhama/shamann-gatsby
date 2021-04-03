6dbc27ebbfa96459a259f1b0df030811af411016
Sylvain Hamann <sylvhama@gmail.com>
2021-04-03 12:47:28 -0400

    Update relevant-experience.js

--- src/pages/relevant-experience.js
+++ src/pages/relevant-experience.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import SEO from '../components/SEO';
-import Paragraph from '../components/shared/Paragraph';
+import SEO from "../components/SEO";
+import Paragraph from "../components/shared/Paragraph";
 
 export default () => (
   <>
@@ -9,14 +9,14 @@ export default () => (
     <Paragraph>
       I graduated from <em>Lumière University Lyon 2</em> in 2013. I have a
       Bachelor’s degree <small>(in France we call that a Licence)</small> in
-      statistics and computer science and a <strong>Master’s degree</strong> in
-      multimedia. During my studies, I have interned at four different companies
-      in France, Luxembourg and South Korea. I am really used to work in a
-      multicultural environment.
+      statistics and computer science. I also have a{" "}
+      <strong>Master’s degree</strong> in computer science where I focused on
+      web development. During my studies, I have interned at four different
+      companies in France, Luxembourg and South Korea.
     </Paragraph>
     <Paragraph>
       After I graduated, I have worked as a web developer for two years at
-      Asiance, a digital agency located in <strong>Seoul</strong>. I also have{' '}
+      Asiance, a digital agency located in <strong>Seoul</strong>. I also have{" "}
       <strong>worked in Tokyo</strong> as a web developer for two digital
       agencies. I initially joined Netwise for eight months and then I worked at
       Cream for roughly one year. I've made web applications and responsive
@@ -34,7 +34,8 @@ export default () => (
       main menu of Assassin's Creed Origins and Odyssey.
     </Paragraph>
     <Paragraph>
-      Nowadays I am working at <strong>Shopify</strong> as a web developer.
+      Nowadays I am working at <strong>Shopify</strong> as a web developer,
+      trying to make commerce better for everyone.
     </Paragraph>
   </>
 );
