31cafd0acc21e9192d9752998a559a61636ee546
Sylvain Hamann <sylvain.hamann@shopify.com>
2022-01-06 20:58:50 -0500

    ah

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,10 @@ export default function Index() {
       <Seo title="About Me" keywords={['react', 'javascript', 'web']} />
       <Paragraph>
         I am from France, I <span role="img" aria-label="love">💖</span> crafting stuff in the Web. 
-        If I had to list my favorite tools and libraries in no particular order it would be: React, React Testing Library, TypeScript, GraphQL and Styled Components.
-        The <abbr title="Keep it Simple, Stupid">KISS</abbr> principle is my mojo. I also try my best to act as an accessibility advocate.
+        If I had to list my favorite tools and libraries in no particular order it would be: React, 
+        React Testing Library, TypeScript, Styled Components and GraphQL (I still love <abbr title="Backends for Frontends">BFF</abbr> pattern).
+        The <abbr title="Keep it Simple, Stupid">KISS</abbr> principle is my mojo. 
+        I also try my best to act as an accessibility advocate.
       </Paragraph>
     </>
   );
