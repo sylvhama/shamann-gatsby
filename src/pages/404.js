16f93b4d4730f02772f90f7ac838a735cb66cd64
Sylvain Hamann <sylvain.hamann@shopify.com>
2022-01-09 14:27:28 -0500

    fix img

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,21 @@
 import React from "react";
-import { StaticImage } from "gatsby-plugin-image";
+import styled from "styled-components";
 
 import Seo from "../components/Seo";
+import gif from "../images/travolta.gif";
+
+const Img = styled.img`
+  margin: 0 auto;
+  max-width: 260px;
+  width: 100%;
+  display: block;
+`;
 
 export default function NotFoundPage() {
   return (
-    <div style={{ textAlign: "center" }}>
+    <>
       <Seo title="404: Not found" />
-      <StaticImage
-        src="../images/travolta.gif"
-        placeholder="blurred"
-        layout="fixed"
-        width={280}
-        quality={100}
-        alt="Confused John Travolta scene from Pulp Fiction"
-      />
-    </div>
+      <Img alt="Confused John Travolta scene from Pulp Fiction" src={gif} />
+    </>
   );
 }
