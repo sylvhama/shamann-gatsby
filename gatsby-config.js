24b3a0b2d5439e3bd724e56e09485d414b65ab1f
Sylvain Hamann <sylvain.hamann@shopify.com>
2022-01-09 10:34:29 -0500

    add gatsby-plugin-react-helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
