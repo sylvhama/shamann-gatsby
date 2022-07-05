5ec10b10405d3d5398cb24c4b876dc82f4f6b360
Sylvain Hamann <sylvain.hamann@shopify.com>
2022-07-05 11:32:21 -0400

    forgot this details

--- src/pages/relevant-experience.js
+++ src/pages/relevant-experience.js
@@ -32,7 +32,7 @@ export default function Experience() {
         Jul 2022 - Present <span aria-hidden="true">🌐 📊</span>
       </SectionSubtitle>
       <Paragraph>
-        I am currently working for <strong>Meta</strong> as a front-end developer.
+        I am currently working for <strong>Meta</strong> as a senior front-end developer.
       </Paragraph>
 
       <SectionSubtitle>
