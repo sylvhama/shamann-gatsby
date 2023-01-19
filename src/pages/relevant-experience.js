798b9d0eb7b56997a6364dbcad3767d09f32c624
Sylvain Hamann <sylvain.hamann@shopify.com>
2023-01-18 19:11:52 -0500

    update meta xp

--- src/pages/relevant-experience.js
+++ src/pages/relevant-experience.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import differenceInYears from 'date-fns/differenceInYears';
-import differenceInMonths from 'date-fns/differenceInMonths';
+import React from "react";
+import differenceInYears from "date-fns/differenceInYears";
+import differenceInMonths from "date-fns/differenceInMonths";
 
-import { Section } from 'components/Section';
-import { SectionSubtitle } from 'components/shared/SectionSubtitle';
-import { Anchor } from 'components/shared/Anchor';
-import { VisuallyHidden } from 'components/shared/VisuallyHidden';
-import { Split } from 'components/shared/Split';
-import { Paragraph } from 'components/shared/Paragraph';
+import { Section } from "components/Section";
+import { SectionSubtitle } from "components/shared/SectionSubtitle";
+import { Anchor } from "components/shared/Anchor";
+import { VisuallyHidden } from "components/shared/VisuallyHidden";
+import { Split } from "components/shared/Split";
+import { Paragraph } from "components/shared/Paragraph";
 
 // September 1st 2013 (0 = january)
 const startDate = new Date(2013, 8, 1);
@@ -32,7 +32,14 @@ export default function Experience() {
       </SectionSubtitle>
       <Paragraph>
         I'm working at <strong>Meta</strong> as a senior front-end developer on
-        the Commerce Insights team.
+        the Commerce Insights team. I contributed to the Facebook & Instagram
+        app on Shopify, we built three new cards showing different insights. For
+        each card, I created dashboards and alerts in order to monitor their
+        health and performance. I fixed different types of issues (responsive,{" "}
+        <abbr title="internationalization">i18n</abbr>,{" "}
+        <abbr title="accessibility">a11y</abbr>...) in the Commerce Insights
+        platform. I also helped to improve the test coverage by implementing new
+        testing utilities and several <abbr title="end to end">e2e</abbr> tests.
       </Paragraph>
 
       <SectionSubtitle>
@@ -42,10 +49,10 @@ export default function Experience() {
         I worked for <strong>Shopify</strong> in their Montreal office then
         remotely after COVID. I contributed to merchant facing experiences
         (Shopify Admin, Polaris design system) and buyer facing experiences
-        (Customer Accounts) while acting as an{' '}
+        (Customer Accounts) while acting as an{" "}
         <Anchor href="https://dev.to/sylvhama/how-to-test-your-web-app-accessibility-26di">
           accessibility advocate
-        </Anchor>{' '}
+        </Anchor>{" "}
         within my team. I mentored developers via code reviews and pair
         programming. I also championed projects with multidisciplinary teams.
       </Paragraph>
@@ -68,11 +75,11 @@ export default function Experience() {
       </SectionSubtitle>
       <Paragraph>
         After I graduated, I have worked as a web developer for two years at
-        Asiance, a digital agency located in <strong>Seoul</strong>. I also have{' '}
+        Asiance, a digital agency located in <strong>Seoul</strong>. I also have{" "}
         worked in <strong>Tokyo</strong> as a web developer for two digital
         agencies. I initially joined Netwise for eight months and then I worked
         at Cream for roughly one year. During my time in Asia I've contributed
-        to roughly <strong>30 web applications and responsive websites</strong>{' '}
+        to roughly <strong>30 web applications and responsive websites</strong>{" "}
         for worldwide clients such as Lacoste or Air France.
       </Paragraph>
 
@@ -80,7 +87,7 @@ export default function Experience() {
         Studies <span aria-hidden="true">🇫🇷 🎓</span>
       </SectionSubtitle>
       <Paragraph>
-        I graduated from <em>Lumière University Lyon 2</em> in 2013. I have a{' '}
+        I graduated from <em>Lumière University Lyon 2</em> in 2013. I have a{" "}
         <strong>Master’s degree</strong> in computer science where I focused on
         web development. I also have a License 3 (undergraduate degree) in data
         science. I have interned at four different companies in France,
@@ -92,11 +99,11 @@ export default function Experience() {
 
 function createSentenceEnd(months) {
   if (months === 0) {
-    return '.';
+    return ".";
   }
 
   if (months === 1) {
-    return ' and 1 month.';
+    return " and 1 month.";
   }
 
   return ` and ${months} months.`;
