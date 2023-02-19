ca2bcbff4ed4c6f14004a9a6f2baf65e05840bb1
Sylvain Hamann <sylvain.hamann@shopify.com>
2023-02-19 13:16:23 -0500

    add links

--- src/pages/articles-talks.js
+++ src/pages/articles-talks.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
-import { Section } from 'components/Section';
-import { Anchor } from 'components/shared/Anchor';
-import { SectionSubtitle } from 'components/shared/SectionSubtitle';
-import { List } from 'components/shared/List';
-import { Paragraph } from 'components/shared/Paragraph';
+import { Section } from "components/Section";
+import { Anchor } from "components/shared/Anchor";
+import { SectionSubtitle } from "components/shared/SectionSubtitle";
+import { List } from "components/shared/List";
+import { Paragraph } from "components/shared/Paragraph";
 
 export default function Talks() {
   return (
@@ -24,42 +24,48 @@ export default function Talks() {
       <Paragraph
         className="reset"
         style={{
-          textAlign: 'left',
+          textAlign: "left",
         }}
       >
         I have talked at several meetups in Seoul, Tokyo and Montreal. I also
         have co-organized public and internal meetups at Ubisoft Montreal and
-        Shopify. I sometimes record{' '}
+        Shopify. I sometimes record{" "}
         <Anchor href="https://www.youtube.com/playlist?list=PL3jMq0AMfCCeiXYJFp0HNLCR5uMZsMPs7">
           videos in French
-        </Anchor>{' '}
+        </Anchor>{" "}
         about my experience as a front-end developer.
       </Paragraph>
 
       <SectionSubtitle>My articles</SectionSubtitle>
       <List>
         <li>
+          <Anchor href="https://dev.to/sylvhama/use-css-to-pass-an-element-through-your-letters-2ad3">
+            Use CSS to pass an element through your letters
+          </Anchor>{" "}
+          - Feb 1, 2023
+        </li>
+        <li>
           <Anchor href="https://dev.to/sylvhama/how-to-test-your-web-app-accessibility-26di">
             How to test your web app accessibility?
-          </Anchor>{' '}
+          </Anchor>{" "}
           - Jun 21, 2022
         </li>
         <li>
           <Anchor href="https://github.com/sylvhama/pwa-links">
             Progressive Web Apps links
-          </Anchor>{' '}
+          </Anchor>{" "}
           - Jun 17, 2020
         </li>
         <li>
           <Anchor href="https://dev.to/sylvhama/multi-directional-navigation-31k2">
             Multi Directional Navigation
-          </Anchor>{' '}
+          </Anchor>{" "}
           - Jun 8, 2020
         </li>
         <li>
           <Anchor href="https://github.com/sylvhama/modern-js">
             Road to Modern JavaScript and React
-          </Anchor>{' '}
+          </Anchor>{" "}
           - Nov 8, 2017
         </li>
       </List>
@@ -67,28 +73,34 @@ export default function Talks() {
       <SectionSubtitle>My talks</SectionSubtitle>
       <List>
         <li>
+          <Anchor href="https://github.com/sylvhama/intro-to-styled-components">
+            Introduction to Styled Components
+          </Anchor>{" "}
+          - MTL FED Talks (Shopify) - Aug 19, 2019
+        </li>
+        <li>
           <Anchor href="https://github.com/sylvhama/bringing-the-www-to-the-aaa">
             Bringing the WWW to the AAA
-          </Anchor>{' '}
+          </Anchor>{" "}
           - Let's React Meetup at Breather (MTL) - Jan 11, 2019
         </li>
         <li>
           <Anchor href="https://github.com/sylvhama/introduction-to-pwa">
             Introduction to Progressive Web Apps
-          </Anchor>{' '}
+          </Anchor>{" "}
           - JS Montreal Meetup at La Gare - Nov 14, 2017
         </li>
         <li>
           <Anchor href="https://github.com/sylvhama/slides/tree/master/chausettes">
             Put your sockets on, a small intro to socket.io and rooms in
             Node.js®
-          </Anchor>{' '}
+          </Anchor>{" "}
           - TokyoJS Meetup at Tokyo Square Garden - Dec 14, 2015
         </li>
         <li>
           <Anchor href="https://github.com/sylvhama/slides/tree/master/phaser">
             My first game with Phaser
-          </Anchor>{' '}
+          </Anchor>{" "}
           - TokyoJS Meetup at 21 Cafe - Sep 14, 2015
         </li>
         <li>
