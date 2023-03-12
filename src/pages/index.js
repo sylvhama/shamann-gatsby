92844b05e72ba19620428b81b4f94b189f42dc64
Sylvain Hamann <sylvain.hamann@shopify.com>
2023-03-12 11:02:09 -0400

    fix SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,17 @@ import React from "react";
 
 import { Section } from "components/Section";
 import { Paragraph } from "components/shared/Paragraph";
+import { VisuallyHidden } from "components/shared/VisuallyHidden";
 
 export default function Index() {
   return (
     <Section title="About Me" keywords={["react", "javascript", "web"]}>
       <Paragraph>
         I’m a French expat working remotely in Montreal. I{" "}
-        <span role="img" aria-label="love">
-          💖
-        </span>{" "}
-        crafting accessible and performant UX with <em>React</em>.
+        <span role="presentation">💖</span>
+        <VisuallyHidden>love</VisuallyHidden> crafting accessible and performant
+        UX with <em>React</em>.
       </Paragraph>
     </Section>
-  ); 
+  );
 }
