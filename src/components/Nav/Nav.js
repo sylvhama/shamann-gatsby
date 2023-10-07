f3a1a048a754d59f3f91b9c564ee4293bf68537a
Sylvain Hamann <sylvhama@gmail.com>
2023-10-07 13:39:11 -0400

    smaller

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,28 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
-import * as styles from './Nav.module.css';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "gatsby";
+import * as styles from "./Nav.module.css";
 
 export function Nav({ links, ...rest }) {
   return (
     <nav {...rest} aria-label="Main navigation">
       <ul className={styles.List}>
         {links.map(({ name, path }) => {
-          const split = name.split(' ');
+          const split = name.split(" ");
           return (
             <li key={name} className={styles.Item}>
               <Link className={styles.Link} to={path} activeClassName="active">
-                {split.map((word, index) => {
-                  if (split.length === 1 || split.length - 1 === index) {
-                    return word;
-                  }
-                  return (
-                    <React.Fragment key={word}>
-                      {word}
-                      <span style={{ fontFamily: 'arial', fontSize: '0.88em' }}> </span>
-                    </React.Fragment>
-                  );
-                })}
+                {word}
               </Link>
             </li>
           );
