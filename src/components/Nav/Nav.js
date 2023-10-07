94b97bbb32614e8c6f2c3caaa3f67737ba22673a
sylvhama <sylvhama@gmail.com>
2023-10-07 13:40:59 -0400

    oops

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,18 +1,17 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Link } from "gatsby";
-import * as styles from "./Nav.module.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import * as styles from './Nav.module.css';
 
 export function Nav({ links, ...rest }) {
   return (
     <nav {...rest} aria-label="Main navigation">
       <ul className={styles.List}>
         {links.map(({ name, path }) => {
-          const split = name.split(" ");
           return (
             <li key={name} className={styles.Item}>
               <Link className={styles.Link} to={path} activeClassName="active">
-                {word}
+                {name}
               </Link>
             </li>
           );
