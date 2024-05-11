434dcb8738510c7ac2ec47d3986b144b7c33952d
Sylvain Hamann <sylvhama@gmail.com>
2024-05-11 08:32:30 -0400

    canada

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,8 @@ export default function Index() {
   return (
     <Section title="About Me" keywords={["react", "javascript", "web"]}>
       <Paragraph>
-        I’m a French expat working remotely in Montreal. I{" "}
+        I’m a French expat who has obtained Canadian citizenship, currently
+        residing in Montreal and working remotely. I{" "}
         <span role="presentation">💖</span>
         <VisuallyHidden>love</VisuallyHidden> crafting accessible and performant
         UX with <em>React</em>.
